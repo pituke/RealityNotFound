cc293583c33833d081ec31accd191f4e505289ca
Stefan <pitukemaster@gmail.com>
2016-02-29 00:26:37 +0100

    InvocationGraph - partial implementation

--- include/Importer/Parsing/InvocationGraph.h
+++ include/Importer/Parsing/InvocationGraph.h
@@ -29,6 +29,7 @@ namespace Importer
 			QVector<GetterSetterDirectAttrAccess> gettersSetters;
 			QVector<MethodInvocation> internalMethods;
 			QVector<MethodInvocation> interfaceMethods;
+			QVector<MethodInvocation> constructors;
 
 			static InvocationGraph AnalyzeClass(const Class& class1)
 			{
@@ -37,10 +38,31 @@ namespace Importer
 				QMap<QString, GetterSetterDirectAttrAccess> gettersSettersMap;
 				QMap<QString, MethodInvocation> internalMethodsMap;
 				QMap<QString, MethodInvocation> interfaceMethodsMap;
-				QMap<QString, uint> allMethods;
+				QMap<QString, MethodInvocation> constructorsMap;
+				QMap<const Method&, uint> allMethods;
 
 				for (uint i = 0; i < class1.methods.count(); ++i)
-					allMethods.insert(class1.methods[i].name, i);
+					allMethods.insert(class1.methods[i], i);
+
+				QMapIterator<const Method&, uint> m(allMethods);
+				while (m.hasNext())
+				{
+					// cekni ci je to getter setter - nazov get/set, vnutri return atribut - spoj ho z atributom s ktorym pracuje
+					const auto& method = m.key();
+					if (method.name.toLower().startsWith("get") || method.name.toLower().startsWith("set"))
+					{
+						QRegExp re("return\\w+;");
+						int pos = 0;
+						while ((pos = re.indexIn(method.content, pos)) != -1)
+						{
+							
+							pos += re.matchedLength();
+						}
+					}
+
+					// cekni ci je to constructor, public, private/protected metoda a podla toho zarad
+					// cekni volania metod, get/setterov, atributov a podla toho napln asociacie - cekuj relativne k zoznamom znamych metod v mapach
+				}
 
 				return res;
 			}
